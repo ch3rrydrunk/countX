98292dcc557f19a1e7996bea715f737292ca1c4c
David Ambartsumian <de1f364@gmail.com>
2019-06-15 19:13:39 +0300

    v1 fail

--- main.py
+++ main.py
@@ -28,13 +28,36 @@ execution_path = os.getcwd()
 
 ######### LOGICS ########
 #~~~~~~~ Commands ~~~~~~#
+
+
 def start(update, context):
+	update.message.reply_text("Сколько примерно человек будет на вашем фото?",
+								reply_markup=start_menu_markup())
+	return MAIN
+
+
+def first_menu(update, context):
 	update.message.reply_text("Начнем?\n"
 								"Просто сфотографируй группу,"
-								"чтобы быстро посчитать количество человек!\n"
-								"Или выбери тип предметов, чтобы посчитать что-то другое!",
+								"чтобы быстро посчитать количество человек!\n",
 								reply_markup=markup)
-	return MAIN
+	return MENU
+
+
+def second_menu(update, context):
+	update.message.reply_text("Начнем?\n"
+								"Просто сфотографируй группу,"
+								"чтобы быстро посчитать количество человек!\n",
+								reply_markup=markup)
+	return MENU_10
+
+
+def third_menu(update, context):
+	update.message.reply_text("Начнем?\n"
+								"Просто сфотографируй группу,"
+								"чтобы быстро посчитать количество человек!\n",
+								reply_markup=markup)
+	return MENU_50
 
 
 def help(update, context):
@@ -65,7 +88,53 @@ def count_x(update, context):
 	file = open('imgnew.jpg', 'rb')
 	i = 0
 	for each in detections:
-		if (each["name"] == "person"):
+		if (each["name"] == "person" and each["percentage_probability"] > 60):
+			i += 1
+	update.message.reply_text("На фото - {} объектов\n".format(i),
+								reply_markup=markup)
+	update.message.reply_photo(file)
+	return MAIN
+
+
+def count_x_10(update, context):
+	image_id = update.message.photo[-1]
+	image = bot_core.bot.get_file(image_id)
+	image.download('img.jpg')
+	while True:
+		if os.path.isfile("img.jpg") == True:
+			break
+	detector = ObjectDetection()
+	detector.setModelTypeAsRetinaNet()
+	detector.setModelPath(os.path.join(execution_path, "resnet50_coco_best_v2.0.1.h5"))
+	detector.loadModel()
+	detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, "img.jpg"), output_image_path=os.path.join(execution_path , "imgnew.jpg"))
+	file = open('imgnew.jpg', 'rb')
+	i = 0
+	for each in detections:
+		if (each["name"] == "person" and each["percentage_probability"] > 40):
+			i += 1
+	update.message.reply_text("На фото - {} объектов\n".format(i),
+								reply_markup=markup)
+	update.message.reply_photo(file)
+	return MAIN
+
+
+def count_x_100(update, context):
+	image_id = update.message.photo[-1]
+	image = bot_core.bot.get_file(image_id)
+	image.download('img.jpg')
+	while True:
+		if os.path.isfile("img.jpg") == True:
+			break
+	detector = ObjectDetection()
+	detector.setModelTypeAsRetinaNet()
+	detector.setModelPath(os.path.join(execution_path, "resnet50_coco_best_v2.0.1.h5"))
+	detector.loadModel()
+	detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, "img.jpg"), output_image_path=os.path.join(execution_path , "imgnew.jpg"))
+	file = open('imgnew.jpg', 'rb')
+	i = 0
+	for each in detections:
+		if (each["name"] == "person" and each["percentage_probability"] > 10):
 			i += 1
 	update.message.reply_text("На фото - {} объектов\n".format(i),
 								reply_markup=markup)
@@ -97,6 +166,15 @@ def error(update, context):
 	"""Log Errors caused by Updates."""
 	logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+####### REPLY_MARKUP #######
+
+
+def start_menu_markup():
+	keyboard = [[InlineKeyboardButton('1⃣ До 10 человек', callback_data='m1')],
+				[InlineKeyboardButton('2⃣ До 50 человек', callback_data='m2')],
+				[InlineKeyboardButton('3⃣ Больше 50 человек', callback_data='m3')]]
+	return InlineKeyboardMarkup(keyboard)
+
 
 ####### IGNITION #######
 # To set API token set env variable (do "export BOT_API_TOKEN=your_token")
@@ -111,14 +189,25 @@ reply_keyboard = [['📲 Свяжись с нами! 📲']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
 #˜˜˜˜˜˜  MANAGER ˜˜˜˜˜˜#
-MAIN = range(1)
+MENU, MENU_10, MENU_50, MAIN = range(4)
 
 conv_handler = ConversationHandler(
 	entry_points=[CMH('start', start)],
 
 	states={
-		MAIN:	[MSH(Filters.regex('^📲 Свяжись с нами! 📲$'), to_contact),
-					MSH(Filters.photo, count_x)]
+		MENU:	[MSH(Filters.regex('^📲 Свяжись с нами! 📲$'), to_contact),
+					MSH(Filters.photo, count_x)],
+
+		MENU_10: [MSH(Filters.regex('^📲 Свяжись с нами! 📲$'), to_contact),
+			   MSH(Filters.photo, count_x_10)],
+
+		MENU_50: [MSH(Filters.regex('^📲 Свяжись с нами! 📲$'), to_contact),
+			   MSH(Filters.photo, count_x_100)],
+
+		MAIN:	[CallbackQueryHandler(first_menu, pattern='m1'),
+					CallbackQueryHandler(second_menu, pattern='m2'),
+					CallbackQueryHandler(third_menu, pattern='m3')],
+
 	},
 	fallbacks=[CMH('cancel', cancel)]
 	)
